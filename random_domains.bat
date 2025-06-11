ebd3286d266d80d56e68a2956420a277f1f2e54d
Halim <halim-abz@users.noreply.github.com>
2025-06-11 11:25:41 +0400

    Update random_domains.bat

--- random_domains.bat
+++ random_domains.bat
@@ -2,18 +2,18 @@
 setlocal enabledelayedexpansion
 
 for /L %%i in (1,1,20) do (
-    REM Generate random 10-character subdomain name
-    set "subdomain="
+    REM Generate random 20-character domain name
+    set "domain="
     for /L %%j in (1,1,10) do (
         set /a "rand=!random! %% 26"
         for %%k in (!rand!) do (
             set "char=abcdefghijklmnopqrstuvwxyz"
             set "char=!char:~%%k,1!"
-            set "subdomain=!subdomain!!char!"
+            set "domain=!domain!!char!"
         )
     )
     
-    set "fulldomain=!subdomain!.test123.com"
+    set "fulldomain=!domain!.com"
     echo Attempt %%i: !fulldomain!
     nslookup -timeout=1 !fulldomain! >nul 2>&1
 )
