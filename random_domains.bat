6898f8d376c9f059e898f1a1aa07158be7de22ff
Halim <halim-abz@users.noreply.github.com>
2025-06-11 10:57:36 +0400

    Create random_domains.bat

---
+++ random_domains.bat
@@ -0,0 +1,20 @@
+@echo off
+setlocal enabledelayedexpansion
+
+for /L %%i in (1,1,20) do (
+    REM Generate random 20-character domain name
+    set "domain="
+    for /L %%j in (1,1,20) do (
+        set /a "rand=!random! %% 26"
+        for %%k in (!rand!) do (
+            set "char=abcdefghijklmnopqrstuvwxyz"
+            set "char=!char:~%%k,1!"
+            set "domain=!domain!!char!"
+        )
+    )
+    
+    set "fulldomain=!domain!.com"
+    echo Attempt %%i: !fulldomain!
+    PING -n 1 !fulldomain! >nul 2>&1   
+    timeout /t 1 >nul
+)
